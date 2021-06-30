24e5ee37f637713a3df3854b2770346862221343
kumkumsingh <kumkumsingh.r@gmail.com>
2021-07-01 00:52:22 +0200

    added pagination

--- src/components/ShipmentList.tsx
+++ src/components/ShipmentList.tsx
@@ -1,14 +1,19 @@
 import * as React from 'react'
 import { Header } from 'semantic-ui-react'
-import { ShipmentType } from '../interfaces/shipment'
 import { fetchShipment } from '../services/shipment'
-import { useEffect, useContext } from 'react'
+import { useEffect, useContext, useState } from 'react'
 import { AppContext } from '../provider/storeContext'
-import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css'
 import ShipmentListItem from './ShipmentListItem'
+import './style.css'
 
 const ShipmentList = () => {
   const { state, dispatch } = useContext(AppContext)
+  const [currentPage, setcurrentPage] = useState<number>(1)
+  const itemsPerPage = 2
+  const pageNumberLimit = 5
+  const [maxPageNumberLimit, setmaxPageNumberLimit] = useState<number>(2)
+  const [minPageNumberLimit, setminPageNumberLimit] = useState<number>(0)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -17,20 +22,90 @@ const ShipmentList = () => {
     }
     fetchData()
   }, [dispatch])
+
+  useEffect(() => {
+    setcurrentPage(1)
+  }, [state.shipments])
+
+  const pages = []
+  for (let i = 1; i <= Math.ceil(state.shipments.length / itemsPerPage); i++) {
+    pages.push(i)
+  }
+
+  const indexOfLastItem = currentPage * itemsPerPage
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage
+  const currentItems = state.shipments.slice(indexOfFirstItem, indexOfLastItem)
+
+  const handleClick = (id: string) => {
+    setcurrentPage(Number(id))
+  }
+
+  const renderPageNumbers = pages.map((pageNum) => {
+    if (parseInt(`${pageNum}`) < maxPageNumberLimit + 1 && parseInt(`${pageNum}`) > minPageNumberLimit) {
+      return (
+        <li
+          key={pageNum}
+          id={`${pageNum}`}
+          onClick={() => handleClick(`${pageNum}`)}
+          className={currentPage === parseInt(`${pageNum}`) ? 'active' : undefined}
+        >
+          {pageNum}
+        </li>
+      )
+    } else {
+      return null
+    }
+  })
+
+  const handleNextbtn = () => {
+    setcurrentPage(currentPage + 1)
+
+    if (currentPage + 1 > maxPageNumberLimit) {
+      setmaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit)
+      setminPageNumberLimit(minPageNumberLimit + pageNumberLimit)
+    }
+  }
+
+  const handlePrevbtn = () => {
+    setcurrentPage(currentPage - 1)
+
+    if ((currentPage - 1) % pageNumberLimit === 0) {
+      setmaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit)
+      setminPageNumberLimit(minPageNumberLimit - pageNumberLimit)
+    }
+  }
+
+  const renderData = currentItems.map((item, i) => {
+    return (
+      <div key={i}>
+        <ShipmentListItem shipment={item} />
+      </div>
+    )
+  })
+
   return (
     <React.Fragment>
       <Header as="h2" textAlign="center" color="blue">
         Your shipment lists
       </Header>
-      {state?.shipments?.map((shipment: ShipmentType) => {
-        return (
-            <div key={shipment.id}>
-         
-            <ShipmentListItem shipment={shipment}/>
-    
-          </div>
-        )
-      })}
+      <div className="detail-conatiner">{renderData}</div>
+      {currentItems.length >= 1 ? (
+        <ul className="pageNumbers">
+          <li>
+            <button onClick={handlePrevbtn} disabled={currentPage === pages[0]}>
+              Prev
+            </button>
+          </li>
+          {renderPageNumbers}
+          <li>
+            <button onClick={handleNextbtn} disabled={currentPage === pages[pages.length - 1]}>
+              Next
+            </button>
+          </li>
+        </ul>
+      ) : (
+        <p> Oops !! No results found. Search or Filter for channels &#x1F60a;</p>
+      )}
     </React.Fragment>
   )
 }
